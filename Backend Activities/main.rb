50901b821243001de217f29c06ba10243afcd731
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-01-27 09:49:27 +0800

    Update main.rb
    wrong file uploader

--- Backend Activities/main.rb
+++ Backend Activities/main.rb
@@ -1,53 +1,67 @@
 module AdminPermission
-    def edit_users_profile
-      puts "Admin updated all users profile"
-    end
-  
-    def change_password
-      puts "Admin changed password"
-    end
+  def edit_users_profile
+    puts "Admin updated all users profile"
+  end
+
+  def change_password(new_password)
+    @password = new_password
+    puts "Admin changed password"
+  end
+
+  def admin_login
+    puts "Admin is logged in"
+  end
 end
-  
+
 module BuyerPermission
-    def buy
-      puts "Buyer has bought an item"
-    end
+  def buy
+    puts "Buyer has bought an item"
+  end
+
+  def buyer_login
+    puts "Buyer is logged in."
+  end
+
+  def change_password(new_password)
+    @password = new_password
+    puts "Buyer changed password"
+  end
 end
-  
+
 class User
-    attr_accessor :username, :password, :ip_address
-    
-    def initialize(username, password, ip_address)
-      @username = username
-      @password = password
-      @ip_address = ip_address
-    end
-  
-    protected
-    def login
-      puts "User logged in. IP address: #@{ip_address}"
-    end
+  attr_accessor :username, :password, :ip_address
+
+  def initialize(username, password, ip_address)
+    @username = username
+    @password = password
+    @ip_address = ip_address
+  end
+
+  protected
+  def login
+    puts "User logged in. IP address: #@{ip_address}"
+  end
 end
-  
-  
+
+
 class Admin < User
-    include AdminPermission
+  include AdminPermission
 end
-  
-  
+
+
 class Buyer < User
-    include BuyerPermission
+  include BuyerPermission
 end
-  
-  
-  my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
-  my_admin.admin_login
-  my_admin.edit_users_profile
-  
-  my_admin.change_password = 'new_password'
-  
-  buyer = Buyer.new('juan', 'password', '127.0.0.1')
-  buyer.buyer_login
-  buyer.buy
-  
-  buyer.change_password = 'new_password'
+
+
+my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
+my_admin.admin_login
+my_admin.edit_users_profile
+
+my_admin.change_password('new_password')
+
+buyer = Buyer.new('juan', 'password', '127.0.0.1')
+buyer.buyer_login
+buyer.buy
+
+buyer.change_password('new_password')

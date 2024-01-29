db3fb7f6cef77bb9d83d5407d2a65558f28cf15f
Paulene Pallan <73598647+paulenepal@users.noreply.github.com>
2024-01-29 20:57:01 +0800

    Add files via upload

---
+++ Backend Activities/main.sql
@@ -0,0 +1,31 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    middle_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    age INT,
+    location TEXT NOT NULL
+);
+
+INSERT INTO students (id, first_name, middle_name, last_name, age, location)
+VALUES
+    (1, 'Juan', '', 'Cruz', 18, 'Manila'),
+    (2, 'Anne', '', 'Wall', 20, 'Manila'),
+    (3, 'Theresa', '', 'Joseph', 21, 'Manila'),
+    (4, 'Isaac', '', 'Gray', 19, 'Laguna'),
+    (5, 'Zack', '', 'Matthews', 22, 'Marikina'),
+    (6, 'Fin', '', 'Lam', 25, 'Manila');
+
+UPDATE students
+SET first_name = 'Pau', middle_name = '', last_name = 'Pallan', age = 25, location = 'Cavite'
+WHERE id = 1;
+
+DELETE FROM students WHERE id = (SELECT MAX(id) FROM students);
+
+SELECT COUNT(*) AS student_count FROM students;
+
+SELECT * FROM students WHERE location = 'Manila';
+
+SELECT AVG(age) FROM students;
+
+SELECT * FROM students ORDER BY (age) DESC;
